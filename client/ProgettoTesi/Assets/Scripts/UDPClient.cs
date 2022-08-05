a214225183c7afc20f279e16bcc977f445e88bd3
Filippo Venturini - filippo.venturini8@studio.unibo.it <filippo.venturini8@studio.unibo.it>
2022-08-05 10:41:53 +0200

    Add logger

--- client/ProgettoTesi/Assets/Scripts/UDPClient.cs
+++ client/ProgettoTesi/Assets/Scripts/UDPClient.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Net;
 #if UNITY_EDITOR
@@ -13,78 +14,60 @@ using System.Threading.Tasks;
 using System.IO;
 using Windows.Networking;
 #endif
- 
- 
- 
+
+
+#if UNITY_EDITOR
+
 public class UDPClient : MonoBehaviour, IDisposable
 {
-    public static UDPClient instance;
- 
     //OnMessageReceived
     public delegate void AddOnMessageReceivedDelegate(string message, IPEndPoint remoteEndpoint);
     public event AddOnMessageReceivedDelegate MessageReceived;
     private void OnMessageReceivedEvent(string message, IPEndPoint remoteEndpoint)
     {
- 
         if (MessageReceived != null)
-        {
             MessageReceived(message, remoteEndpoint);
- 
-        }
     }
- 
-#if UNITY_EDITOR
- 
+
     private Thread _ReadThread;
     private UdpClient _Socket;
- 
-#else
- 
-    DatagramSocket _Socket = null;
- 
-#endif
- 
-    #region COMMON
-    string messageReceived = "";
-    public TMP_Text txtLog;
-    private bool isReceiving = false;
 
     private GameObject cube;
+    private GameObject debugGameObj;
+    private TextMeshProUGUI debugConsole;
     private float lastRotation;
     private float rotationReceived;
- 
-    private void Awake()
-    {
-        instance = this;
-    }
- 
+
+    private bool newLog = false;
+    private string logMsg;
+
+
     void Start()
     {
         Receive(8051);
         cube = GameObject.Find("Cube");
-        InvokeRepeating("OnBtn_FreeMemory", 2, 5);
+        debugGameObj = GameObject.Find("DebugTxt");
+        debugConsole = debugGameObj.GetComponent<TextMeshProUGUI>();
+        debugConsole.text = "Your Text";
     }
-    
+
     void Update()
     {
-        rotationReceived = float.Parse(messageReceived);
-        //print(messageReceived);
         if(lastRotation != rotationReceived)
         {
+            if(newLog){
+                debugConsole.text = logMsg;
+                newLog = false;
+            }
+            
             cube.transform.Rotate(rotationReceived,0,0);
             lastRotation = rotationReceived;
         }
-        //txtLog.text = messageReceived;
     }
-    #endregion
- 
-#if UNITY_EDITOR
- 
+
+
     public void Receive(int port)
     {
-        if (isReceiving) return;
-        isReceiving = true;
- 
         // create thread for reading UDP messages
         _ReadThread = new Thread(new ThreadStart(delegate
         {
@@ -105,12 +88,14 @@ public class UDPClient : MonoBehaviour, IDisposable
                     // receive bytes
                     IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
                     byte[] data = _Socket.Receive(ref anyIP);
- 
+
                     // encode UTF8-coded bytes to text format
                     string message = Encoding.UTF8.GetString(data);
+                    
+                    NewLog(message);
+
+                    rotationReceived = float.Parse(message);
                     OnMessageReceivedEvent(message, anyIP);
-                    messageReceived = message;
- 
                 }
                 catch (Exception err)
                 {
@@ -121,7 +106,7 @@ public class UDPClient : MonoBehaviour, IDisposable
         _ReadThread.IsBackground = true;
         _ReadThread.Start();
     }
- 
+
     public void Dispose()
     {
         if (_ReadThread.IsAlive)
@@ -133,38 +118,85 @@ public class UDPClient : MonoBehaviour, IDisposable
             _Socket.Close();
             _Socket = null;
         }
-        isReceiving = false;
     }
- 
- 
+
+    private void NewLog(string msg){
+        newLog = true;
+        logMsg = msg;
+    }
+}
+
 #else
- 
+
+public class UDPClient : MonoBehaviour, IDisposable
+{
+    //OnMessageReceived
+    public delegate void AddOnMessageReceivedDelegate(string message, IPEndPoint remoteEndpoint);
+    public event AddOnMessageReceivedDelegate MessageReceived;
+    private void OnMessageReceivedEvent(string message, IPEndPoint remoteEndpoint)
+    {
+        if (MessageReceived != null)
+            MessageReceived(message, remoteEndpoint);
+    }
+
+
+    DatagramSocket _Socket = null;
+    private GameObject cube;
+    private GameObject debugGameObj;
+    private TextMeshProUGUI debugConsole;
+    private float lastRotation;
+    private float rotationReceived;
+
+    private bool newLog = false;
+    private string logMsg;
+
+    void Start()
+    {
+        Receive(8051);
+        cube = GameObject.Find("Cube");
+        debugGameObj = GameObject.Find("DebugTxt");
+        debugConsole = debugGameObj.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if(lastRotation != rotationReceived)
+        {
+            if(newLog){
+                debugConsole.text = logMsg;
+                newLog = false;
+            }
+
+            cube.transform.Rotate(rotationReceived,0,0);
+            lastRotation = rotationReceived;
+        }
+    }
+
     public async void Receive(int port)
     {
-        if (isReceiving) return;
- 
-        isReceiving = true;
-   
+        NewLog("ENTRATO IN RECEIVE");
         string portStr = port.ToString();
         // start the client
         try
         {
             _Socket = new DatagramSocket();
             _Socket.MessageReceived += _Socket_MessageReceived;
- 
+
             await _Socket.BindServiceNameAsync(portStr);
- 
+
             //await _Socket.BindEndpointAsync(null, portStr);
- 
+
             //await _Socket.ConnectAsync(new HostName("255.255.255.255"), portStr.ToString());
- 
- 
+
+
             //HostName hostname = Windows.Networking.Connectivity.NetworkInformation.GetHostNames().FirstOrDefault();
             //var ep = new EndpointPair(hostname, portStr, new HostName("255.255.255.255"), portStr);
             //await _Client.ConnectAsync(ep);
- 
-            Debug.Log(string.Format("Receiving on {0}", portStr));
- 
+
+            //Debug.Log(string.Format("Receiving on {0}", portStr));
+
+            NewLog(string.Format("Receiving on {0}", portStr));
+
             await Task.Delay(3000);
             // send out a message, otherwise receiving does not work ?!
             var outputStream = await _Socket.GetOutputStreamAsync(new HostName("255.255.255.255"), portStr);
@@ -176,29 +208,30 @@ public class UDPClient : MonoBehaviour, IDisposable
         {
             _Socket.Dispose();
             _Socket = null;
+            NewLog(ex.ToString());
             Debug.LogError(ex.ToString());
             Debug.LogError(Windows.Networking.Sockets.SocketError.GetStatus(ex.HResult).ToString());
         }
     }
- 
+
     private async void _Socket_MessageReceived(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs args)
     {
         try
         {
             Stream streamIn = args.GetDataStream().AsStreamForRead();
             StreamReader reader = new StreamReader(streamIn, Encoding.UTF8);
- 
+
             string message = await reader.ReadLineAsync();
+            rotationReceived = float.Parse(message);
             IPEndPoint remoteEndpoint = new IPEndPoint(IPAddress.Parse(args.RemoteAddress.RawName), Convert.ToInt32(args.RemotePort));
             OnMessageReceivedEvent(message, remoteEndpoint);
-            messageReceived = message;
         }
         catch (Exception ex)
         {
             Debug.LogError(ex.ToString());
         }
     }
- 
+
     public void Dispose()
     {
         if (_Socket != null)
@@ -206,27 +239,13 @@ public class UDPClient : MonoBehaviour, IDisposable
             _Socket.Dispose();
             _Socket = null;
         }
-        isReceiving = false;
     }
-#endif
- 
-    public void OnBtn_FreeMemory()
-    {
-        GC.Collect();
-        Resources.UnloadUnusedAssets();
+
+    private void NewLog(string msg){
+        newLog = true;
+        logMsg = msg;
     }
-    public void OnBtn_StartSocket()
-    {
-        Receive(9999);
-    }
-    public void OnBtn_CloseSocket()
-    {
-        Dispose();
-    }
- 
-    private void OnDisable()
-    {
-        Dispose();
-    }
- 
 }
+
+#endif
+
