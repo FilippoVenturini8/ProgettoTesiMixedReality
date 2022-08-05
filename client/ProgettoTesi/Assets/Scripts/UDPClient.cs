b27b0964e888710ee3a21cd3d7dc1eec0d6b09b0
Filippo Venturini - filippo.venturini8@studio.unibo.it <filippo.venturini8@studio.unibo.it>
2022-08-05 11:06:43 +0200

    try to make client work

--- client/ProgettoTesi/Assets/Scripts/UDPClient.cs
+++ client/ProgettoTesi/Assets/Scripts/UDPClient.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Net;
+using System.Collections.Generic;
+using UnityEngine.Events;
 #if UNITY_EDITOR
 using System.Net.Sockets;
 using System.Threading;
 #else
 using Windows.Networking.Sockets;
+using Windows.Networking.Connectivity;
 using Windows.Storage.Streams;
 using System.Threading.Tasks;
 using System.IO;
@@ -128,116 +131,143 @@ public class UDPClient : MonoBehaviour, IDisposable
 
 #else
 
-public class UDPClient : MonoBehaviour, IDisposable
+[System.Serializable]
+public class UDPMessageEvent : UnityEvent<string, string, byte[]>
 {
-    //OnMessageReceived
-    public delegate void AddOnMessageReceivedDelegate(string message, IPEndPoint remoteEndpoint);
-    public event AddOnMessageReceivedDelegate MessageReceived;
-    private void OnMessageReceivedEvent(string message, IPEndPoint remoteEndpoint)
+
+}
+
+public class UDPClient : MonoBehaviour, Singleton<UDPCommunication>
+{
+   [Tooltip ("port to listen for incoming data")]
+    public string internalPort = "8051";
+
+    [Tooltip("IP-Address for sending")]
+    public string externalIP = "192.168.17.110";
+
+    [Tooltip("Port for sending")]
+    public string externalPort = "12346";
+
+    [Tooltip("Send a message at Startup")]
+    public bool sendPingAtStart = true;
+
+    [Tooltip("Conten of Ping")]
+    public string PingMessage = "hello";
+
+    [Tooltip("Function to invoke at incoming packet")]
+    public UDPMessageEvent udpEvent = null;
+
+    private readonly  Queue<Action> ExecuteOnMainThread = new Queue<Action>();
+
+    //we've got a message (data[]) from (host) in case of not assigned an event
+    void UDPMessageReceived(string host, string port, byte[] data)
     {
-        if (MessageReceived != null)
-            MessageReceived(message, remoteEndpoint);
+        Debug.Log("GOT MESSAGE FROM: " + host + " on port " + port + " " + data.Length.ToString() + " bytes ");
     }
 
-
-    DatagramSocket _Socket = null;
-    private GameObject cube;
-    private GameObject debugGameObj;
-    private TextMeshProUGUI debugConsole;
-    private float lastRotation;
-    private float rotationReceived;
-
-    private bool newLog = false;
-    private string logMsg;
-
-    void Start()
+    //Send an UDP-Packet
+    public async void SendUDPMessage(string HostIP, string HostPort, byte[] data)
     {
-        Receive(8051);
-        cube = GameObject.Find("Cube");
-        debugGameObj = GameObject.Find("DebugTxt");
-        debugConsole = debugGameObj.GetComponent<TextMeshProUGUI>();
+        await _SendUDPMessage(HostIP, HostPort, data);
     }
 
+    DatagramSocket socket;
+
+    async void Start()
+    {
+        if (udpEvent == null)
+        {
+            udpEvent = new UDPMessageEvent();
+            udpEvent.AddListener(UDPMessageReceived);
+        }
+
+        NewLog("Waiting for a connection...");
+        Debug.Log("Waiting for a connection...");
+
+        socket = new DatagramSocket();
+        socket.MessageReceived += Socket_MessageReceived;
+
+        HostName IP = null;
+        try
+        {
+            var icp = NetworkInformation.GetInternetConnectionProfile();
+
+            IP = Windows.Networking.Connectivity.NetworkInformation.GetHostNames()
+            .SingleOrDefault(
+                hn =>
+                    hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId
+                    == icp.NetworkAdapter.NetworkAdapterId);
+
+            await socket.BindEndpointAsync(IP, internalPort);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            Debug.Log(SocketError.GetStatus(e.HResult).ToString());
+            return;
+        }
+
+        if(sendPingAtStart)
+            SendUDPMessage(externalIP, externalPort, Encoding.UTF8.GetBytes(PingMessage));
+
+    }
+
+    private async System.Threading.Tasks.Task _SendUDPMessage(string externalIP, string externalPort, byte[] data)
+    {
+        using (var stream = await socket.GetOutputStreamAsync(new Windows.Networking.HostName(externalIP), externalPort))
+        {
+            using (var writer = new Windows.Storage.Streams.DataWriter(stream))
+            {
+                writer.WriteBytes(data);
+                await writer.StoreAsync();
+
+            }
+        }
+    }
+
+    static MemoryStream ToMemoryStream(Stream input)
+    {
+        try
+        {                                         // Read and write in
+            byte[] block = new byte[0x1000];       // blocks of 4K.
+            MemoryStream ms = new MemoryStream();
+            while (true)
+            {
+                int bytesRead = input.Read(block, 0, block.Length);
+                if (bytesRead == 0) return ms;
+                ms.Write(block, 0, bytesRead);
+            }
+        }
+        finally { }
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if(lastRotation != rotationReceived)
+        while (ExecuteOnMainThread.Count > 0)
         {
-            if(newLog){
-                debugConsole.text = logMsg;
-                newLog = false;
-            }
+            ExecuteOnMainThread.Dequeue().Invoke();
 
-            cube.transform.Rotate(rotationReceived,0,0);
-            lastRotation = rotationReceived;
         }
     }
 
-    public async void Receive(int port)
+    private void Socket_MessageReceived(Windows.Networking.Sockets.DatagramSocket sender,
+        Windows.Networking.Sockets.DatagramSocketMessageReceivedEventArgs args)
     {
-        NewLog("ENTRATO IN RECEIVE");
-        string portStr = port.ToString();
-        // start the client
-        try
+         Debug.Log("GOT MESSAGE FROM: " + args.RemoteAddress.DisplayName);
+        //Read the message that was received from the UDP  client.
+        Stream streamIn = args.GetDataStream().AsStreamForRead();
+        MemoryStream ms = ToMemoryStream(streamIn);
+        byte[] msgData = ms.ToArray();
+
+        if (ExecuteOnMainThread.Count == 0)
         {
-            _Socket = new DatagramSocket();
-            _Socket.MessageReceived += _Socket_MessageReceived;
-
-            await _Socket.BindServiceNameAsync(portStr);
-
-            //await _Socket.BindEndpointAsync(null, portStr);
-
-            //await _Socket.ConnectAsync(new HostName("255.255.255.255"), portStr.ToString());
-
-
-            //HostName hostname = Windows.Networking.Connectivity.NetworkInformation.GetHostNames().FirstOrDefault();
-            //var ep = new EndpointPair(hostname, portStr, new HostName("255.255.255.255"), portStr);
-            //await _Client.ConnectAsync(ep);
-
-            //Debug.Log(string.Format("Receiving on {0}", portStr));
-
-            NewLog(string.Format("Receiving on {0}", portStr));
-
-            await Task.Delay(3000);
-            // send out a message, otherwise receiving does not work ?!
-            var outputStream = await _Socket.GetOutputStreamAsync(new HostName("255.255.255.255"), portStr);
-            DataWriter writer = new DataWriter(outputStream);
-            writer.WriteString("Hello World!");
-            await writer.StoreAsync();
-        }
-        catch (Exception ex)
-        {
-            _Socket.Dispose();
-            _Socket = null;
-            NewLog(ex.ToString());
-            Debug.LogError(ex.ToString());
-            Debug.LogError(Windows.Networking.Sockets.SocketError.GetStatus(ex.HResult).ToString());
-        }
-    }
-
-    private async void _Socket_MessageReceived(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs args)
-    {
-        try
-        {
-            Stream streamIn = args.GetDataStream().AsStreamForRead();
-            StreamReader reader = new StreamReader(streamIn, Encoding.UTF8);
-
-            string message = await reader.ReadLineAsync();
-            rotationReceived = float.Parse(message);
-            IPEndPoint remoteEndpoint = new IPEndPoint(IPAddress.Parse(args.RemoteAddress.RawName), Convert.ToInt32(args.RemotePort));
-            OnMessageReceivedEvent(message, remoteEndpoint);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError(ex.ToString());
-        }
-    }
-
-    public void Dispose()
-    {
-        if (_Socket != null)
-        {
-            _Socket.Dispose();
-            _Socket = null;
+            ExecuteOnMainThread.Enqueue(() =>
+            {
+                Debug.Log("ENQEUED ");
+                if (udpEvent != null)
+                    udpEvent.Invoke(args.RemoteAddress.DisplayName, internalPort, msgData);
+            });
         }
     }
 
@@ -245,6 +275,7 @@ public class UDPClient : MonoBehaviour, IDisposable
         newLog = true;
         logMsg = msg;
     }
+
 }
 
 #endif
